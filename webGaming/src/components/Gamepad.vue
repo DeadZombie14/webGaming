221d5033937c3dd2664c201468ca85c48eaa93b5
M. Mathier Mendoza Morelos <43389808+DeadZombie14@users.noreply.github.com>
2023-09-30 05:23:12 -0600

    GUI implementation + gamepad conection test + material icons

---
+++ webGaming/src/components/Gamepad.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>       
+    </div>
+</template>
+
+<style scoped>
+/**/
+</style>
+
+<script lang="ts">
+import { ref } from 'vue'
+window.addEventListener("gamepadconnected", (e) => {
+  console.log(
+    "Gamepad connected at index %d: %s. %d buttons, %d axes.",
+    e.gamepad.index,
+    e.gamepad.id,
+    e.gamepad.buttons.length,
+    e.gamepad.axes.length,
+  );
+})
+</script>
--- webGaming/src/views/Home.vue
+++ webGaming/src/views/Home.vue
@@ -1,18 +1,92 @@
 <template>
-  <div id="screenContainer" ref="mainScreen">
-    <video ref="screen" autoplay muted></video>
-    <div class="videoOverlay">
-      <button class="btn playBtn" @click="play()" v-if="videoStreamSource == null">Start transmision</button>
+  <main>
+    <div class="left-sidebar">
+      <button class="sidebar-btn" style="background-color: darkviolet;" @click="copyShareLink">
+        <span class="material-symbols-outlined">offline_share</span>
+      </button>
+      <button class="sidebar-btn" style="background-color: darkviolet;">
+        <span class="material-symbols-outlined">settings</span>
+      </button>
     </div>
-  </div>
+    <div id="screenContainer" ref="mainScreen">
+      <video ref="screen" autoplay muted></video>
+      <div class="videoOverlay">
+        <button class="btn playBtn" @click="play()" v-if="videoStreamSource == null && !hostID">Start transmision</button>
+        <span v-if="videoStreamSource == null && hostID">Waiting for host to start...</span>
+      </div>
+    </div>
+    <div class="right-sidebar">
+      <button class="sidebar-btn" style="background-color: orange;">
+        <span>Player 1</span>
+        <span class="material-symbols-outlined" style="font-size: 2rem;">sports_esports</span>
+      </button>
+      <button class="sidebar-btn" style="background-color: red;">
+        <span>Player 2</span>
+        <span class="material-symbols-outlined" style="font-size: 2rem;">sports_esports</span>
+      </button>
+      <button class="sidebar-btn" style="background-color: green;">
+        <span>Player 3</span>
+        <span class="material-symbols-outlined" style="font-size: 2rem;">sports_esports</span>
+      </button>
+      <button class="sidebar-btn" style="background-color: blue;">
+        <span>Player 4</span>
+        <span class="material-symbols-outlined" style="font-size: 2rem;">sports_esports</span>
+      </button>
+    </div>
+  </main>
+  <Gamepad></Gamepad>
 </template>
 
 <style scoped>
-#screenContainer {
-  padding: 1rem;
-  width: 100%;
-  height: 100vh;
+main {
+  padding: 1rem 0 1rem 0;
+  display: flex;
+  gap: .5rem;
   background: var(--mainColor);
+  height: 100vh;
+}
+.left-sidebar {
+  height: 100%;
+  margin-right: auto;
+  display: grid;
+  gap: 1rem;
+  justify-content: space-around;
+}
+.right-sidebar {
+  height: 100%;
+  margin-left: auto;
+  display: grid;
+  gap: 1rem;
+  justify-content: space-around;
+}
+.sidebar-btn {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  padding: .5rem;
+  white-space: nowrap;
+  cursor: pointer;
+  border: 1px solid black;
+  transition: all .2s;
+  overflow: hidden;
+}
+.sidebar-btn.disabled {
+  background-color: gray !important;
+}
+.left-sidebar .sidebar-btn {
+  border-radius: 0 1rem 1rem 0;
+}
+.right-sidebar .sidebar-btn {
+  border-radius: 1rem 0 0 1rem;
+}
+.left-sidebar .sidebar-btn:hover {
+  background-color: violet !important;
+}
+
+#screenContainer {
+  width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -26,7 +100,7 @@
   border: 1rem solid black;
   border-radius: 1rem;
 }
-.videoOverlay {
+#screenContainer .videoOverlay {
   position: absolute;
   width: 100%;
   height: 100%;
@@ -35,6 +109,7 @@
   display: flex;
   justify-content: center;
   align-items: center;
+  color: #fff;
 }
 .playBtn {
   
@@ -42,11 +117,12 @@
 </style>
 
 <script setup lang="ts">
+import '../components/Gamepad.vue'
 import { ref, computed } from "vue"
-import { useRoute } from 'vue-router';
+import { useRoute } from 'vue-router'
 const route = useRoute()
 
-import { Peer } from "peerjs"
+import { Peer, DataConnection } from "peerjs"
 
 // Query params
 const hostID = route.params.id as string | null
@@ -55,22 +131,25 @@ const hostID = route.params.id as string | null
 const screen = ref<HTMLVideoElement | null>(null)
 
 // Main code
-const connected: string[] = []
+let shareURL = ""
+const connected: DataConnection[] = []
 let mediaStreamCalls: any[] = []
 let videoStreamSource = ref<MediaStream | null>(null)
 
 const peer = new Peer()
+let conn: DataConnection | null = null // keeps connection ref if we are clients
 peer.on('open',function(id) {
   if (!hostID) {
     // We are the master, listen for calls
-    console.log('Join via link: ' + window.location.origin + '/room/' + id)
+    shareURL = window.location.origin + '/room/' + id
+    console.log('Join via link: ' + shareURL)
     peer.on('connection', function(conn) {
       conn.on('open', function() {
         conn.send(`welcome ${conn.peer} !`)
         if(videoStreamSource.value) {
             mediaStreamCalls.push(peer.call(conn.peer, videoStreamSource.value))
         }
-        connected.push(conn.peer) // Add to connected list
+        connected.push(conn) // Add to connected list
       })
       
       // Receive messages
@@ -92,13 +171,13 @@ peer.on('open',function(id) {
   } else {
     // We are "clients", connect to master
     console.log('My client ID is: '+ id)
-    const conn = peer.connect(hostID)
+    conn = peer.connect(hostID)
     conn.on('open', function() {
       // Send messages
-      conn.send('Hello!')
+      conn!.send('Hello!')
       
       // Receive messages
-      conn.on('data', function(data) {
+      conn!.on('data', function(data) {
         console.log(`Master says: ${data}`)
       })
     })
@@ -112,6 +191,10 @@ peer.on('open',function(id) {
         // videotrack.applyConstraints({ frameRate: { min: 60 } })
         screen.value!.srcObject = remoteStream
         videoStreamSource.value = remoteStream
+        // If video stream ended, nullify source
+        remoteStream.getVideoTracks()[0].addEventListener('ended', () => {
+          videoStreamSource.value = null
+        })
       })
       
       // Video stream ended
@@ -131,8 +214,8 @@ function play() {
     videoStreamSource.value = stream
     
     // Video stream started, create one mediaStreamCall per client
-    for (let peerID of connected) {
-      mediaStreamCalls.push(peer.call(peerID, stream))
+    for (let conn of connected) {
+      mediaStreamCalls.push(peer.call(conn.peer, stream))
     }
 
     // Video stream ended, kill all mediaStreamCalls
@@ -144,6 +227,10 @@ function play() {
       videoStreamSource.value = null
     })
   })
+}
+
+function copyShareLink() {
+  navigator.clipboard.writeText(shareURL)
 }
 
 </script>
